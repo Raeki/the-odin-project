403682a8e0cdc9053286f2d7d86527aec38b5b93
Matthew K <mkeithley@gmail.com>
2022-05-02 15:39:02 +0900

    create text for prompt

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -8,11 +8,11 @@ function computerPlay() {
 
 // prompt the human player for their choice and return a sanitized version
 function playerSelection(){
-  const playerChoice = prompt();
+  const playerChoice = prompt('Please type rock, paper, or scissors:);
   if (options.some(prompt)) {
   return prompt.toLowerCase();
   } else {
-    throw 'Please choose rock, paper, or scissors';
+    throw 'Please choose rock, paper, or scissors.';
   }
 }
 
