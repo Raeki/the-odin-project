e7d6772ca0d46ea1598799bdb015518203c49265
Matthew K <mkeithley@gmail.com>
2022-05-02 15:32:58 +0900

    add comments

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,5 +1,7 @@
+// create array of valid inputs
 const options = ["rock", "paper", "scissors"];
 
+// returns a random input for the computer to play
 function computerPlay() {
   return options[Math.floor(Math.random() * options.length)]
 }
