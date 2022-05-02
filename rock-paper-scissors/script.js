5d0dfd05fb8e5a84dacc2f5ac7111a03e1f4dbe4
Matthew K <mkeithley@gmail.com>
2022-05-02 15:37:40 +0900

    add player selection prompt

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -5,3 +5,14 @@ const options = ["rock", "paper", "scissors"];
 function computerPlay() {
   return options[Math.floor(Math.random() * options.length)]
 }
+
+// prompt the human player for their choice and return a sanitized version
+function playerSelection(){
+  const playerChoice = prompt();
+  if (options.some(prompt)) {
+  return prompt.toLowerCase();
+  } else {
+    throw 'Please choose rock, paper, or scissors';
+  }
+}
+
